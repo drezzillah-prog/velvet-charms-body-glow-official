4f391a2526cee8a6bc0f5985435aeafe63fb3b86
drezzillah-prog <drezzillah@gmail.com>
2026-08-23 14:09:49 +0300

    Complete remaining Body Glow artist specialty translations

--- about-specialties-multilingual.js
+++ about-specialties-multilingual.js
@@ -3,18 +3,24 @@
   'use strict';
   const maps={
     fr:{
+      'Fine Art & Custom Painting':'Beaux-arts & peinture sur mesure',
+      'Candle Making & Wax Craft':'Création de bougies & travail de la cire',
       'Needle-Felted Animals & Keepsakes':'Animaux feutrés à l’aiguille & objets-souvenirs',
       'Botanical Skincare & Body Care':'Soins botaniques du visage & du corps',
       'Knitted & Braided Textile Art':'Art textile tricoté & tressé',
       'Perfumery & Scent Design':'Parfumerie & création olfactive'
     },
     it:{
+      'Fine Art & Custom Painting':'Belle arti & pittura su misura',
+      'Candle Making & Wax Craft':'Creazione di candele & lavorazione della cera',
       'Needle-Felted Animals & Keepsakes':'Animali infeltriti ad ago & oggetti-ricordo',
       'Botanical Skincare & Body Care':'Skincare botanica & cura del corpo',
       'Knitted & Braided Textile Art':'Arte tessile lavorata a maglia & intrecciata',
       'Perfumery & Scent Design':'Profumeria & creazione olfattiva'
     },
     de:{
+      'Fine Art & Custom Painting':'Bildende Kunst & individuelle Malerei',
+      'Candle Making & Wax Craft':'Kerzenherstellung & Wachshandwerk',
       'Needle-Felted Animals & Keepsakes':'Nadelgefilzte Tiere & Erinnerungsstücke',
       'Botanical Skincare & Body Care':'Botanische Haut- & Körperpflege',
       'Knitted & Braided Textile Art':'Gestrickte & geflochtene Textilkunst',
